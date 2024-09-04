74a7e168b656b0946faa182cbd175f06e0dedf84
Sergei Popinevskii <gurza000@gmail.com>
2024-09-04 14:25:16 +0300

    init s3fs

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gurza/go-billy-s3fs
+
+go 1.21.9
+
+require github.com/go-git/go-billy/v5 v5.6.0
---
+++ pkg/s3fs/s3fs.go
@@ -0,0 +1,88 @@
+package s3fs
+
+import (
+	"io/fs"
+
+	"github.com/go-git/go-billy/v5"
+)
+
+type S3FS struct{}
+
+func NewS3FS() (billy.Filesystem, error) {
+	return &S3FS{}, nil
+}
+
+// Chroot implements billy.Filesystem.
+func (s *S3FS) Chroot(path string) (billy.Filesystem, error) {
+	panic("unimplemented")
+}
+
+// Create implements billy.Filesystem.
+func (s *S3FS) Create(filename string) (billy.File, error) {
+	panic("unimplemented")
+}
+
+// Join implements billy.Filesystem.
+func (s *S3FS) Join(elem ...string) string {
+	panic("unimplemented")
+}
+
+// Lstat implements billy.Filesystem.
+func (s *S3FS) Lstat(filename string) (fs.FileInfo, error) {
+	panic("unimplemented")
+}
+
+// MkdirAll implements billy.Filesystem.
+func (s *S3FS) MkdirAll(filename string, perm fs.FileMode) error {
+	panic("unimplemented")
+}
+
+// Open implements billy.Filesystem.
+func (s *S3FS) Open(filename string) (billy.File, error) {
+	panic("unimplemented")
+}
+
+// OpenFile implements billy.Filesystem.
+func (s *S3FS) OpenFile(filename string, flag int, perm fs.FileMode) (billy.File, error) {
+	panic("unimplemented")
+}
+
+// ReadDir implements billy.Filesystem.
+func (s *S3FS) ReadDir(path string) ([]fs.FileInfo, error) {
+	panic("unimplemented")
+}
+
+// Readlink implements billy.Filesystem.
+func (s *S3FS) Readlink(link string) (string, error) {
+	panic("unimplemented")
+}
+
+// Remove implements billy.Filesystem.
+func (s *S3FS) Remove(filename string) error {
+	panic("unimplemented")
+}
+
+// Rename implements billy.Filesystem.
+func (s *S3FS) Rename(oldpath string, newpath string) error {
+	panic("unimplemented")
+}
+
+// Root implements billy.Filesystem.
+func (s *S3FS) Root() string {
+	panic("unimplemented")
+}
+
+// Stat implements billy.Filesystem.
+func (s *S3FS) Stat(filename string) (fs.FileInfo, error) {
+	panic("unimplemented")
+}
+
+// Symlink implements billy.Filesystem.
+func (s *S3FS) Symlink(target string, link string) error {
+	panic("unimplemented")
+}
+
+// TempFile implements billy.Filesystem.
+func (s *S3FS) TempFile(dir string, prefix string) (billy.File, error) {
+	panic("unimplemented")
+}
