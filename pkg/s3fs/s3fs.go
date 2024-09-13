41f43dc4ea8d85105f2f2ddab5066d50e1d08c07
Sergei Popinevskii <gurza000@gmail.com>
2024-09-13 12:32:35 +0300

    implement ReadDir method

--- pkg/s3fs/s3fs.go
+++ pkg/s3fs/s3fs.go
@@ -1,20 +1,39 @@
 package s3fs
 
 import (
+	"context"
 	"fmt"
 	"io/fs"
+	"os"
 	"path"
 	"strings"
+	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/go-git/go-billy/v5"
 )
 
 type S3FS struct {
-	root string
+	client *s3.S3
+	bucket string
+	root   string
 }
 
-func NewS3FS() (billy.Filesystem, error) {
-	return &S3FS{}, nil
+// NewS3FS creates a new S3-backed filesystem for the given bucket.
+func New(client *s3.S3, bucket string) (billy.Filesystem, error) {
+	if client == nil {
+		return nil, fmt.Errorf("s3 client cannot be nil")
+	}
+	if bucket == "" {
+		return nil, fmt.Errorf("bucket name cannot be empty")
+	}
+
+	return &S3FS{
+		client: client,
+		bucket: bucket,
+		root:   "/",
+	}, nil
 }
 
 // Chroot creates a new filesystem rooted at newRoot within the current root.
@@ -71,9 +90,65 @@ func (s *S3FS) OpenFile(filename string, flag int, perm fs.FileMode) (billy.File
 	panic("unimplemented")
 }
 
-// ReadDir implements billy.Filesystem.
-func (s *S3FS) ReadDir(path string) ([]fs.FileInfo, error) {
-	panic("unimplemented")
+// ReadDir lists the contents of a directory in the S3 bucket,
+// returning file and directory information.
+func (s *S3FS) ReadDir(name string) ([]os.FileInfo, error) {
+	cleanName := path.Clean(name)
+	if path.IsAbs(cleanName) {
+		cleanName = cleanName[1:]
+	}
+
+	// Combine with the root path and normalize
+	s3Path := path.Join(s.root, cleanName)
+	s3Path = strings.TrimLeft(s3Path, "/")
+	if s3Path != "" && !strings.HasSuffix(s3Path, "/") {
+		s3Path += "/"
+	}
+
+	// Ensure the path doesn't escape the root
+	base := path.Clean(strings.TrimLeft(s.root, "/"))
+	target := path.Clean(strings.TrimLeft(path.Join(s.root, cleanName), "/"))
+	if !isSubPath(base, target) {
+		return nil, fmt.Errorf("invalid path: %s escapes from root", name)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	input := &s3.ListObjectsV2Input{
+		Bucket:    aws.String(s.bucket),
+		Delimiter: aws.String("/"),
+	}
+	if s3Path != "" && s3Path != "/" {
+		input.Prefix = aws.String(s3Path)
+	}
+
+	var results []os.FileInfo
+	err := s.client.ListObjectsV2PagesWithContext(ctx, input, func(page *s3.ListObjectsV2Output, last bool) bool {
+		for _, prefix := range page.CommonPrefixes {
+			dirName := strings.TrimPrefix(*prefix.Prefix, s3Path)
+			dirName = strings.TrimSuffix(dirName, "/")
+			if dirName != "" && dirName != "/" {
+				results = append(results, newDirInfo(dirName))
+			}
+		}
+		for _, obj := range page.Contents {
+			fileName := strings.TrimPrefix(*obj.Key, s3Path)
+			if fileName != "" && !strings.HasSuffix(fileName, "/") {
+				results = append(results, newFileInfo(
+					fileName,
+					*obj.Size,
+					*obj.LastModified,
+				))
+			}
+		}
+		return !last
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list objects: %w", err)
+	}
+
+	return results, nil
 }
 
 // Readlink implements billy.Filesystem.
