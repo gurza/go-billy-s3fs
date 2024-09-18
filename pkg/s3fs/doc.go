3512cb0e3be0ce43860012db67aaaa6171528e21
Sergei Popinevskii <gurza000@gmail.com>
2024-09-18 10:12:59 +0300

    docs: actualize package review

--- pkg/s3fs/doc.go
+++ pkg/s3fs/doc.go
@@ -1,6 +1,9 @@
-// Package s3fs provides an implementation of the billy.Filesystem interface
-// for interacting with an object storage bucket (e.g. Amazon S3 bucket)
-// as if it were a traditional file system. This allows seamless integration
-// with applications that rely on the billy abstraction, enabling operations
-// such as reading, writing, deleting, and traversing files and directories.
+// Package s3fs implements the billy.Filesystem interface for interacting
+// with an Amazon S3 bucket as a virtual filesystem. This enables seamless
+// integration with applications relying on the billy abstraction, supporting
+// operations like reading, writing, deleting, and traversing files and dirs.
+//
+// Note: When working with paths in this package, always use the `path` package
+// instead of `filepath`, as S3 paths are UNIX-like and must use forward
+// slashes (/).
 package s3fs
